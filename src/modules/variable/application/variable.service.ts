9747caf5626a733e13674191a7ed7a486fa2d13d
Santiago Restrepo <srestrepo@abatechnology.com>
2024-10-15 15:59:39 -0500

    refactor: change way we save block results

--- src/modules/variable/application/variable.service.ts
+++ src/modules/variable/application/variable.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { VariableRepository } from '../domain/variable.repository';
-import { Variable } from '../domain/variable.entity';
+import { Variable, VariableValue } from '../domain/variable.entity';
 import Block from 'src/modules/block/domain/block.entity';
 
 @Injectable()
@@ -11,51 +11,12 @@ export class VariableService {
     return this.variableRepository.find();
   }
 
-  createFromBlockOutput(block: Block, object: any) {
-    const prefix = `block_${block.id}_output`;
-    const flatObject = this.#flatObject(object, prefix);
-    const variables = this.#flatObjectToVariables(block, flatObject);
-    return this.variableRepository.save(variables);
-  }
-
-  #flatObjectToVariables(block: Block, object: any) {
-    return Object.keys(object).map((key) => {
-      const value = object[key];
-      const variable = new Variable();
-      variable.key = key;
-      variable.value = value;
-      variable.block = block;
-      return variable;
-    });
-  }
-
-  #flatObject(obj: any, prefix = '') {
-    const result = {};
-    const stack = [{ parentKey: prefix, value: obj }];
-
-    while (stack.length > 0) {
-      const { parentKey, value } = stack.pop();
-
-      if (typeof value === 'object' && !Array.isArray(value)) {
-        // If it's an object, push its properties to the stack
-        for (const key in value) {
-          if (value.hasOwnProperty(key)) {
-            const newKey = parentKey ? `${parentKey}.${key}` : key;
-            stack.push({ parentKey: newKey, value: value[key] });
-          }
-        }
-      } else if (Array.isArray(value)) {
-        // Handle arrays by appending the index
-        value.forEach((item, index) => {
-          const arrayKey = `${parentKey}.${index + 1}`;
-          stack.push({ parentKey: arrayKey, value: item });
-        });
-      } else {
-        // If it's a primitive value, store it
-        result[parentKey] = value;
-      }
-    }
-
-    return result;
+  createFromBlockOutput(block: Block, blockResults: VariableValue) {
+    const key = `block_${block.id}_output`;
+    const variable = new Variable();
+    variable.key = key;
+    variable.value = blockResults;
+    variable.block = block;
+    return this.variableRepository.save(variable);
   }
 }
--- src/modules/variable/domain/variable.entity.ts
+++ src/modules/variable/domain/variable.entity.ts
@@ -1,15 +1,15 @@
 import Block from 'src/modules/block/domain/block.entity';
 import BaseEntity from 'src/shared/base-entity.entity';
 import { Column, Entity, ManyToOne } from 'typeorm';
-
+export type VariableValue = object | string | number | null;
 @Entity()
 //Slug with stepId should be unique
 export class Variable extends BaseEntity {
   @Column({ type: 'varchar', unique: true, length: 255 })
   key: string;
 
-  @Column({ type: 'varchar', length: 255 })
-  value: string;
+  @Column({ type: 'jsonb', nullable: true })
+  value: VariableValue;
 
   @Column({ type: 'int', nullable: true })
   blockId: number | null;
